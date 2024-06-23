0ec46f935e452eba021ef3aa15ae636f7f6aa1a0
pjt3591oo <pjt3591oo@gmail.com>
2024-06-23 14:32:28 +0900

    example에서 chromadb persistence 경로 변경

--- example/vector-query.py
+++ example/vector-query.py
@@ -7,13 +7,14 @@ if (not os.environ['OPENAI_API_KEY']):
 
 # vectorstore 디비 커넥션
 vectorstore = Chroma(
-    persist_directory='../chroma',
+    persist_directory='chroma',
     embedding_function=OpenAIEmbeddings()
 )
 
 docs = vectorstore.similarity_search(
     "python의 비동기 모델을 설명해줘"
 )
+print(docs)
 for doc in docs:
     print(doc.page_content.replace('\n\n\n', '\n'))
     print(doc.metadata)
